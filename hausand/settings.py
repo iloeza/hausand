9cbff489d6e3a4729e09442dd6a3ca7495c8319b
iloeza <loeza1992@gmail.com>
2022-07-29 13:31:04 +0000

    Add gitignore

--- hausand/settings.py
+++ hausand/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-so%mu@q8wme9jrzzc#9f$wl5zbsbtc(rnu+e23mw$+feki$ts4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hausand.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
