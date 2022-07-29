56e718633ba0398ef80900336e163e63bec96349
Ivan Loeza <ijlq_loeza@hotmail.com>
2022-07-29 13:11:01 -0500

    Add partials for modal and book list

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     status = models.BooleanField(default=True)
     nivel = models.CharField(max_length=20, default='kinder')
-    responsable = models.CharField(max_length=100, blank=True)
+    alumno_responsable = models.CharField(max_length=100, blank=True)
     telefono_responsable = models.CharField(max_length=20, blank=True)
     email_responsable = models.EmailField(max_length=100, blank=True)
 
--- books/static/books/js/app.js
+++ books/static/books/js/app.js
@@ -1,4 +1,7 @@
 $(document).on("click", ".open-AddBookDialog", function () {
-    var myBookId = $(this).data('id');
+    let myBookId = $(this).data('id');
+    let myBookName = $(this).data('name');
+    console.log($(this).data('name'))
     $(".modal-body #bookId").val(myBookId);
+    $(".modal-header #books__modalLabel").html(`Donar libro: ${myBookName}`);
 });
