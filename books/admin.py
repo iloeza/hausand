d1a9f83797c11eb2177334997eaf6e2a2b4126df
Ivan Loeza <loeza1992@gmail.com>
2025-07-28 23:20:44 -0600

    feat: add improvements for book model

--- books/admin.py
+++ books/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.models import User, Group
+
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'nivel', 'status', 'alumno_responsable', 'telefono_responsable', 'email_responsable')
+    list_display = (
+        "title",
+        "nivel",
+        "status",
+        "alumno_responsable",
+        "telefono_responsable",
+        "email_responsable",
+    )
+    search_fields = ("title", "alumno_responsable")
+
+    site_header = "Hausand"
+    site_title = "Hausand"
+    index_title = "Hausand"
+
 
 # Register your models here.
 admin.site.register(Book, BookAdmin)
+admin.site.unregister(User)
+admin.site.unregister(Group)
--- books/models.py
+++ books/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+
+class NivelChoices(models.TextChoices):
+    MATERNA = "maternal"
+    KINDER = "kinder"
+    PRIMARIA = "primaria"
+    SECUNDARIA = "secundaria"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     status = models.BooleanField(default=True)
-    nivel = models.CharField(max_length=20, default='kinder')
+    nivel = models.CharField(
+        max_length=20, default=NivelChoices.KINDER, choices=NivelChoices.choices
+    )
     alumno_responsable = models.CharField(max_length=100, blank=True)
     telefono_responsable = models.CharField(max_length=20, blank=True)
     email_responsable = models.EmailField(max_length=100, blank=True)
